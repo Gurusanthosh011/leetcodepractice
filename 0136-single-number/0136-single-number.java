7d4ee05e0bb14728f4a47e142c79ca5f47daa319
Gurusanthosh011 <145849838+Gurusanthosh011@users.noreply.github.com>
2024-10-06 22:29:45 +0530

    Time: 14 ms (14.19%), Space: 44.8 MB (94.9%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,16 +2,14 @@ class Solution {
     public int singleNumber(int[] nums) {
         int num2=0;
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Step 2: Populate the HashMap with the frequency of each number
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Step 3: Find the number with frequency 1
+        
         for (int num : map.keySet()) {
-            if (map.get(num) %2==1) {
-                num2= num;  // This is the number that appears only once
+            if (map.get(num)==1) {
+                num2= num;  
             }
         }
         return num2;
