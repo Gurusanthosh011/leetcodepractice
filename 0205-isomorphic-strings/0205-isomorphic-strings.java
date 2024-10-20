ca0254bfbea10fc38752640080425f5fa01af0bd
Gurusanthosh011 <145849838+Gurusanthosh011@users.noreply.github.com>
2024-10-20 08:44:12 +0530

    Time: 15 ms (39.68%), Space: 42.3 MB (55.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,4 +1,4 @@
-/*import java.util.*;
+import java.util.*;
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length()!=t.length()){
@@ -21,42 +21,5 @@ class Solution {
         }
         return true;
         
-    }
-}*/
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        // If lengths differ, they can't be isomorphic
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        // Create two HashMaps for bidirectional mapping
-        HashMap<Character, Character> mapS = new HashMap<>();
-        HashMap<Character, Character> mapT = new HashMap<>();
-
-        // Loop through characters of both strings
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-
-            // If there's already a mapping for s -> t, check if it matches the current character in t
-            if (mapS.containsKey(charS)) {
-                if (mapS.get(charS) != charT) {
-                    return false;
-                }
-            }
-            // If there's already a mapping for t -> s, check if it matches the current character in s
-            if (mapT.containsKey(charT)) {
-                if (mapT.get(charT) != charS) {
-                    return false;
-                }
-            }
-
-            // Otherwise, create new mappings in both directions
-            mapS.put(charS, charT);
-            mapT.put(charT, charS);
-        }
-
-        return true;
     }
 }
