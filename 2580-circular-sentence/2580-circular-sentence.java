e600f8c32b672b3e420c2d66c7d17247675f7e33
Gurusanthosh011 <145849838+Gurusanthosh011@users.noreply.github.com>
2024-11-02 17:12:08 +0530

    Time: 2 ms (18.09%), Space: 42.1 MB (25.89%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public boolean isCircularSentence(String sentence) {
         String[] s1=sentence.split(" ");
         int k=s1.length;
@@ -14,10 +14,7 @@
                 int h=s1[i].length();
                 char ch=s1[i].charAt(h-1);
                 char ch1=s1[i+1].charAt(0);
-                if(ch==ch1){
-                    return true;
-                }
-                else{
+                if(ch!=ch1){
                     return false;
                 }
                  }
@@ -26,31 +23,5 @@
        return c1==c2;
         
     }
-}*/
-class Solution {
-    public boolean isCircularSentence(String sentence) {
-        String[] s1 = sentence.split(" ");
-        int k = s1.length;
-
-        // Case where there is only one word
-        if (k == 1) {
-            int n = s1[0].length();
-            return s1[0].charAt(0) == s1[0].charAt(n - 1);
-        }
-
-        // Loop through each word to check circular condition
-        for (int i = 0; i < k - 1; i++) {
-            int h = s1[i].length();
-            char ch = s1[i].charAt(h - 1);     // Last character of the current word
-            char ch1 = s1[i + 1].charAt(0);    // First character of the next word
-            
-            // Return false immediately if any pair of words does not match
-            if (ch != ch1) {
-                return false;
-            }
-        }
-
-        // Final check: last character of the last word matches the first character of the first word
-        return s1[k - 1].charAt(s1[k - 1].length() - 1) == s1[0].charAt(0);
     }
-}
+
