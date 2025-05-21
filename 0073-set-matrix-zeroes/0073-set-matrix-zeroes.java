75c0d357b9f660ceaeb8a5979344bf6afcd029f6
Gurusanthosh011 <145849838+Gurusanthosh011@users.noreply.github.com>
2025-05-21 12:30:50 +0530

    Time: 1 ms (72.83%), Space: 46 MB (16.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -32,12 +32,6 @@ class Solution {
             }
           }
           }
-          for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-          }
-
+          
     }
 }
